e8b158519d2625c3fde40a3572d418a9c8e3aaa3
Jarrod Millman <jarrod.millman@gmail.com>
2022-03-07 17:54:22 -0800

    Move js to assets (#219)
    * Move js to assets
    
    * Fix
    
    * Default behavior
    
    * Fix

--- assets/js/youtube_page.js
+++ assets/js/youtube_page.js
@@ -5,10 +5,10 @@ for (i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "none") {
-      content.style.display = "block";
-    } else {
+    if (content.style.display === "block") {
       content.style.display = "none";
+    } else {
+      content.style.display = "block";
     }
   });
 }

a79aac7d86d8b900961fc6bd0dcab340ebf4a0cc
Adam Porter <adam@alphapapa.net>
2023-10-17 19:52:26 -0500

    Meta: Add .dir-locals.el (#503)

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((css-mode . ((css-indent-offset . 2)))
+ (html-mode . ((sgml-basic-offset . 4))))
